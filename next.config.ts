9abca71ed6dcd8ddcda04197e3a3603b88d1dfaa
caetano-rangel <cacabond22@gmail.com>
2025-11-11 17:49:23 -0300

    Configurações

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", //  Gera HTML estático
-  images: {
-    unoptimized: true, //  Necessário para exportar imagens
-  },
-  basePath: "/lp", //  troque pelo nome do seu repositório
-  assetPrefix: "/lp/",
+  
 };
 
 export default nextConfig;
